3f4103c7863b93a40ad8cad274426dc910f9ad2a
Chufretalas <marcoantonio.bene@gmail.com>
2025-03-13 19:07:33 -0300

    added a model selector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./App.module.css"
-import ollama, { GenerateResponse } from "ollama/dist/browser.cjs";
+import ollama from "ollama/dist/browser.cjs";
 
 export default function App() {
     const [title, setTitle] = useState("")
@@ -8,12 +8,16 @@ export default function App() {
 
     const [response, setResponse] = useState("")
 
+    const [currentModel, setCurrentModel] = useState("")
+
+    const [allModels, setAllModels] = useState<string[]>([])
+
     async function checkText() {
 
         setResponse("")
 
         const res = await ollama.generate({
-            model: "mistral-nemo",
+            model: currentModel,
             stream: true,
             prompt: `I am practicing my german writing. Can you please proofread the following text and explain my errors: ${content}`
         })
@@ -24,21 +28,42 @@ export default function App() {
         }
     }
 
+    useEffect(() => {
+        ollama.list().then(res => {
+            setAllModels(res.models.map(m => m.name))
+            if (res.models.length > 0) {
+                setCurrentModel(res.models[0].name)
+            } else {
+                //TODO: instruct user to dowload something
+            }
+        })
+    }, [])
+
     return (
-        <main className={styles.main}>
-            <section>
-                <input type="text" name="title_input" id="title_input"
-                    placeholder="Text title..."
-                    value={title} onChange={e => setTitle(e.currentTarget.value)} />
-                <textarea name="title_input" id="title_input"
-                    placeholder="The text to evaluated..."
-                    value={content} onChange={e => setContent(e.currentTarget.value)} />
-            </section>
-            <section>
-                <button onClick={checkText}>Check</button>
-                <span className={styles.response_box}>{response}</span>
-            </section>
-        </main>
+        <>
+            <header>
+                <div>
+                    <select name="model_select" id="model_select"
+                        value={currentModel} onChange={e => setCurrentModel(e.currentTarget.value)}>
+                        {allModels.map((m, idx) => (<option key={idx} value={m}>{m}</option>))}
+                    </select>
+                </div>
+            </header>
+            <main className={styles.main}>
+                <section>
+                    <input type="text" name="title_input" id="title_input"
+                        placeholder="Text title..."
+                        value={title} onChange={e => setTitle(e.currentTarget.value)} />
+                    <textarea name="title_input" id="title_input"
+                        placeholder="The text to evaluated..."
+                        value={content} onChange={e => setContent(e.currentTarget.value)} />
+                </section>
+                <section>
+                    <button onClick={checkText}>Check</button>
+                    <span className={styles.response_box}>{response}</span>
+                </section>
+            </main>
+        </>
     )
 }
 
